78701dcc6355161272bcde2378fcad15e3636d98
Fabian Quirola <fabian.quirola@kfc.com.ec>
2019-10-22 19:11:48 -0500

    ejercicio2

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.writeHead(200);
-    res.write('Hello World -- express');
-    res.end();
+    res.send('Hello World! --Express ');
 });
 
 app.listen(3000,()=>{
