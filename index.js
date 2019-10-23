ac26cf34b8aa30a9748ddecfa863b472db7c60a4
Fabian Quirola <fabian.quirola@kfc.com.ec>
2019-10-22 19:26:57 -0500

    ejercicio4

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.get('/',(req,res)=>{
+app.use(express.static('public'));
+
+/*app.get('/',(req,res)=>{
     res.sendFile(`${__dirname}/public/index.html`);
+});
+*/
+app.get('/keywords',(req,res)=>{
+    const keywords = ['nodejs','express'];
+    res.send(keywords);
 });
 
 app.listen(3000,()=>{
