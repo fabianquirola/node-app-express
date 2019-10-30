2d9625171febbe0dcc811b610f868bfff2f8efcf
Fabian Quirola <fabian.quirola@kfc.com.ec>
2019-10-30 18:58:10 -0500

    dotenv

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+const dotenv = require('dotenv');
 const express = require('express');
 const middleware = require('./middleware');
 const routes = require('./routes');
 const app = express();
 
 const database = require('./database');
+
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
